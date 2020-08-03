0c1162749a52b79342006f6cede08e887852128a
Sushil <sjsushil09@gmail.com>
2020-08-04 03:04:24 +0530

    Harcoded UserName is removed, works in a generic way with the Logout functionality being enabled(Spring Security).

--- SpringBootFirstApp/src/main/java/com/sjsushil09/SpringBootFirstApp/web/web/controller/WelcomeController.java
+++ SpringBootFirstApp/src/main/java/com/sjsushil09/SpringBootFirstApp/web/web/controller/WelcomeController.java
@@ -1,6 +1,8 @@
 package com.sjsushil09.SpringBootFirstApp.web.web.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -9,21 +11,26 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.SessionAttributes;
 
-import com.sjsushil09.SpringBootFirstApp.web.web.services.LoginService;
 
 @Controller
-@SessionAttributes("name") //to store this info across multiple pages 
-public class LoginController {
-	@Autowired
-	LoginService loginservice;
+//@SessionAttributes("name") //to store this info across multiple pages(We are getting this via Spring Security) 
+public class WelcomeController {
 	
 	@RequestMapping(value="/",method=RequestMethod.GET)//to map to particular url routing & only applicable for get method
 	//@ResponseBody//When we don't have view to render
-	public String showLoginPage(ModelMap map) {
-		map.put("name","sushil");
+	public String showWelcomePage(ModelMap map) {
+		map.put("name",getLoggedinUserName());
 		return "Welcome";
 	}
 	
+	//To get userName via Spring Security
+	private String getLoggedinUserName() {
+		Object principal=SecurityContextHolder.getContext().getAuthentication().getPrincipal();
+		if(principal instanceof UserDetails) {
+			return ((UserDetails)principal).getUsername();
+		}
+		return principal.toString();
+	}
 	
 	//Removed below method to replace it with security method
 	/*
